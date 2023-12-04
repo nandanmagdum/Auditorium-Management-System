490bc8c90c64e5744be6555f619efcc382af717a
nandan <nandanmagdum14@gmail.com>
2023-12-04 15:28:28 +0530

    clashing_event_done

--- lib/backend/clash.dart
+++ lib/backend/clash.dart
@@ -1,32 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EventHelper {
-  static Future<bool> isSlotAvailable(DateTime selectedStartDateTime, DateTime selectedEndDateTime) async {
-    // Reference to the Firestore collection
-    CollectionReference eventsCollection = FirebaseFirestore.instance.collection('finalEvents');
-
-    // Query for events overlapping with the selected time range
-    QuerySnapshot eventsSnapshot = await eventsCollection
-        .where('datetime_start', isGreaterThan: selectedStartDateTime)
-        .where('datetime_end', isLessThan: selectedEndDateTime)
-        .get();
-
-    // Check for clashes with each event
-    for (QueryDocumentSnapshot eventSnapshot in eventsSnapshot.docs) {
-      // Parse start and end times from Firestore document
-      DateTime eventStartDateTime = DateTime.parse(eventSnapshot['datetime_start']);
-      DateTime eventEndDateTime = DateTime.parse(eventSnapshot['datetime_end']);
-
-      // Check for clashes
-      if ((selectedStartDateTime.isAfter(eventStartDateTime) && selectedStartDateTime.isBefore(eventEndDateTime)) ||
-          (selectedEndDateTime.isAfter(eventStartDateTime) && selectedEndDateTime.isBefore(eventEndDateTime)) ||
-          (selectedStartDateTime.isAtSameMomentAs(eventStartDateTime) && selectedEndDateTime.isAtSameMomentAs(eventEndDateTime))) {
-        // Clash found
+  bool isSlotAvailable(List<Timestamp> starting, List<Timestamp> ending,
+      Timestamp s, Timestamp e) {
+    print("isSlotAvailable funtion called");
+    int n = starting.length;
+    int m = ending.length;
+    print(n);
+    print(m);
+    for (var i = 0; i < n; i++) {
+      print("${starting[i]}  - ${ending[i]}  -- ${s} - ${e}");
+      int c1 = starting[i].compareTo(s);
+      int c2 = starting[i].compareTo(e);
+      int c3 = ending[i].compareTo(s);
+      int c4 = ending[i].compareTo(e);
+      if(c2 == 0 || c3 == 0) return true;
+      if ((c1 >= 0 && c1 <= 0) || (c4 >= 0 && c4 <= 0)) {
+        // If the start or end time of the current event is equal to the start or end time of any existing event
+        return false;
+      } else if ((c1 <= 0 && c4 >= 0) || (c1 >= 0 && c4 <= 0)) {
+        // If the current event completely overlaps with an existing event or vice versa
+        return false;
+      } else if ((c1 >= 0 && c3 <= 0) || (c2 >= 0 && c4 <= 0)) {
+        // If the start time of the current event is between the start and end time of an existing event
+        return false;
+      } else if ((c1 <= 0 && c3 >= 0) || (c2 <= 0 && c4 >= 0)) {
+        // If the end time of the current event is between the start and end time of an existing event
         return false;
       }
     }
-
-    // No clashes found
+    print("returning true");
     return true;
   }
 }
